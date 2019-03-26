4f854fee1b699ca967071ecb89f696a565025c78
wrujuta <wpd2@njit.edu>
2019-03-25 21:35:57 -0400

    Added Contact Page

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,12 +2,18 @@
 <html lang="en">
 @include('includes.head')
 <body>
- @include('includes.menu')
- <main role="main">
-     @yield('content')
- </main>
+<div class="container">
+    <header>
+        @include('includes.menu')
+    </header>
+    <main role="main">
+        @yield('content')
+    </main>
 
-@include('includes.jsfooter')
-@include('includes.footer')
+    @include('includes.jsfooter')
+    <footer>
+        @include('includes.footer')
+    </footer>
+</div>
 </body>
 </html>
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.default')
+
+@section('content')
+
+    <h1>Contact</h1>
+    <p class="lead">Please use this form to contact the site owner.</p>
+
+
+@endsection
