a02bd01d09e2b7552f8e245aecbf4ce3958f4d94
wrujuta <wpd2@njit.edu>
2019-03-25 21:49:12 -0400

    Updated Contact Page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -4,6 +4,17 @@
 
     <h1>Contact</h1>
     <p class="lead">Please use this form to contact the site owner.</p>
+    <form>
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+        </div>
+        <div class="form-group">
+            <label for="message">Message</label>
+            <textarea name="message" class="form-control" id="message" rows="3"></textarea>
+        </div>
+    </form>
+
 
 
 @endsection
