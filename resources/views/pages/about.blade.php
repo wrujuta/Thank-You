952cdaa86fd542c60e3b3bedc798bbd0313cb120
wrujuta <wpd2@njit.edu>
2019-03-25 21:22:00 -0400

    Project Setup

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="container">
+        <h1>This is my about page</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+    </div>
+
+@endsection
